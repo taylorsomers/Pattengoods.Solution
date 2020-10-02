6891ac5f07f69851cd7aefad9ccb4b44a6e998da
Taylor Somers <tsomers@yandex.com>
2020-10-02 17:16:12 -0400

    Reformat for legibility and add Microsoft.EntityFrameworkCore using directive

--- Pattengoods/Startup.cs
+++ Pattengoods/Startup.cs
@@ -2,9 +2,12 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Pattengoods.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,9 +17,13 @@ namespace Pattengoods
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
         }
 
         public IConfiguration Configuration { get; }
@@ -24,10 +31,17 @@ namespace Pattengoods
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddEntityFrameworkMySql()
+                .AddDbContext<PattengoodsContext>(
+                    options => options
+                        .UseMySql(Configuration["ConnectionStrings:DefaultConnection"]
+                ));
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
+
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
@@ -45,18 +59,24 @@ namespace Pattengoods
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
+
             app.UseStaticFiles();
+
             app.UseCookiePolicy();
+
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
     }
