79a7082c8ad4d9b97571497bf6685019611dffd1
Taylor Somers <tsomers@yandex.com>
2020-10-02 17:25:46 -0400

    Add UseKestrel(), UseContentRoot(), and UseIISIntegration() methods to CreateWebHostBuilder in Main() and alphabetize using directives

--- Pattengoods/Program.cs
+++ Pattengoods/Program.cs
@@ -1,12 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace Pattengoods
 {
@@ -14,7 +14,12 @@ namespace Pattengoods
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .Build()
+                .Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
