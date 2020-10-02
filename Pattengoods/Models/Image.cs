316840d167c3d3e4201413e9687dbfac10962bd8
Taylor Somers <tsomers@yandex.com>
2020-10-02 12:52:21 -0400

    Add Image class to model with data conversion capacity to retrieve images from database

---
+++ Pattengoods/Models/Image.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Pattengoods.Models
+{
+  public class Image
+  {
+    public byte[] Img { get; set; }
+
+    public int ImageId { get; set; }
+
+    public string ImgTitle { get; set; }
+
+    public Product Product { get; set; }
+
+    public static string RetrieveImage(Image image)
+    {
+      string imageBase64Data = Convert.ToBase64String(image.Img);
+      string imageDataURL = string.Format("data:image/jpg;base64, {0}", imageBase64Data);
+      return imageDataURL;
+    }
+  }
+}
