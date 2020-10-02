1d054b364a79afa825a6ea0137ce96b2a3bcdbc0
Taylor Somers <tsomers@yandex.com>
2020-10-02 13:48:09 -0400

    Add database context with initial seed data to model

---
+++ Pattengoods/Models/PattengoodsContext.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Pattengoods.Models
+{
+  public class PattengoodsContext : IdentityDbContext<User>
+  {
+    public DbSet<Category> Categories { get; set; }
+
+    public DbSet<CategoryProduct> CategoryProduct { get; set; }
+
+    public DbSet<Image> Images { get; set; }
+
+    public DbSet<Product> Products { get; set; }
+
+    public PattengoodsContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      base.OnModelCreating(builder);
+
+      builder.Entity<Category>()
+        .HasData(
+          new Category {
+            CategoryId = 1,
+            CategoryTitle = "Furniture"
+          }
+        );
+
+      builder.Entity<Product>()
+        .HasData(
+          new Product {
+            ProductId = 1,
+            ProductManufacturer = "Thuma", 
+            ProductName = "The Bed",
+            ProductPrice = 995.00m,
+            ProductDescription = "Meet the perfect platform bed frame. Thoughtfully designed and crafted to elevate any bedroom. Includes natural walnut colored frame, PillowBoard and cushion-coated slats.",
+            ProductLink = "https://www.thuma.co/products/the-bed?variant=7550696456220"
+          }
+        );
+    }
+  }
+}
