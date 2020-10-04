882edc25cc2c1c46f66430a4b4fdb5c60098cd0e
Taylor Somers <tsomers@yandex.com>
2020-10-04 15:00:20 -0400

    Add readonly properties, AccountController constructor, and Index method

--- Pattengoods/Controllers/AccountController.cs
+++ Pattengoods/Controllers/AccountController.cs
@@ -8,3 +8,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+
+namespace Pattengoods.Controllers
+{
+  [Authorize]
+  public class AccountController : Controller
+  {
+    private readonly PattengoodsContext _db;
+
+    private readonly SignInManager<User> _signInManager;
+
+    private readonly UserManager<User> _userManager;
+
+    public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, PattengoodsContext db)
+    {
+      _db = db;
+
+      _signInManager = signInManager;
+
+      _userManager = userManager;
+    }
+
+    public async Task<ActionResult> Index()
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+      var currentUser = await _userManager.FindByIdAsync(userId);
+
+      return View(currentUser);
+    }
+  }
+}
