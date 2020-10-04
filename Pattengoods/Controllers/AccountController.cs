6526db426ecb2635da4ba868827c99c2a8945e71
Taylor Somers <tsomers@yandex.com>
2020-10-04 14:01:45 -0400

    Add using directives to controller

---
+++ Pattengoods/Controllers/AccountController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pattengoods.Models;
+using Pattengoods.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
