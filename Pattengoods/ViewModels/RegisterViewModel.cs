e9caf3692530ee831cd5ae5b069bca1adbc80653
Taylor Somers <tsomers@yandex.com>
2020-10-04 14:49:02 -0400

    Add RegisterViewModel class to model

---
+++ Pattengoods/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Pattengoods.ViewModels
+{
+  public class RegisterViewModel
+  {
+    [Required]
+    [Display(Name = "First Name")]
+    public string FirstName { get; set; }
+
+    [Required]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    [Display(Name = "Email")]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    public string Password { get; set; }
+
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm Password")]
+    [Compare("Password", ErrorMessage = "Password and Confirm Password entries must match.")]
+    public string ConfirmPassword { get; set; }
+  }
+}
