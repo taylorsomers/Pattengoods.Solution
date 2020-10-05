4bbfb5ea13ca63244f68424839cadf257bb2b1cf
Taylor Somers <tsomers@yandex.com>
2020-10-05 19:22:44 -0400

    Add namespace, class, and Context methods

--- Pattengoods/Controllers/CategoriesController.cs
+++ Pattengoods/Controllers/CategoriesController.cs
@@ -7,3 +7,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+namespace Pattengoods.Controllers
+{
+  public class CategoriesController : Controller
+  {
+    private readonly PattengoodsContext _db;
+
+    public CategoriesController(PattengoodsContext db)
+    {
+      _db = db;
+    }
+
+    
+  }
+}
