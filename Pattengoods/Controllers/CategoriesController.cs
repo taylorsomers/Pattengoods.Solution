c1c6f60309547cccc5b0904ae0bccb4089ed5b3e
Taylor Somers <tsomers@yandex.com>
2020-10-05 19:34:03 -0400

    Add Detail HTTPGet method

--- Pattengoods/Controllers/CategoriesController.cs
+++ Pattengoods/Controllers/CategoriesController.cs
@@ -23,5 +23,38 @@ namespace Pattengoods.Controllers
       List<Category> model = _db.Categories.ToList();
       return View(model);
     }
+
+    public ActionResult Details(int id)
+    {
+      var thisCategory = _db.Categories
+        .Include(category => category.Products)
+        .ThenInclude(join => join.Product)
+        .ThenInclude(product => product.Images)
+        .FirstOrDefault(category => category.CategoryId == id);
+      
+      List<string> productNames = new List<string>();
+
+      Dictionary<string, string> productImages = new Dictionary<string, string>();
+
+      foreach(var join in thisCategory.Products)
+      {
+        if (join.Product.Images.Any())
+        {
+          string ImageData = Image.RetrieveImage(join.Product.Images[0]);
+
+          productNames.Add(join.Product.ProductName);
+
+          productImages.Add(join.Product.ProductName, ImageData);
+        }
+      }
+
+      ViewBag.ProductNames = productNames;
+
+      ViewBag.ImageDictionary = productImages;
+
+      return View(thisCategory);
+    }
+
+    
   }
 }
